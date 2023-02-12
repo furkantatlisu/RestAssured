b486ae2b91a6d89f975b1491dce7e2062856ea1f
FURKAN TATLISU <EXT02D51010@turkcell.entp.tgc>
2023-02-12 20:40:11 +0300

    comments added

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -1,4 +1,11 @@
 public class Basics {
+    public static void main(String[] args) {
+        //validate if Add place API is working as expected
+        //given -- all input details
+        //when -- submit the API
+        //then -- validate the response
 
+
+    }
 
 }
